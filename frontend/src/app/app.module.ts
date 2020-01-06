bfaaf30f4841405c221b400aef990630c127eee3
Radhika P <radhika.p@incedoinc.com>
2020-01-06 21:56:35 +0530

    experiment with flex box

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './shared/material.module'
 import { FormsModule } from '@angular/forms';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlexboxComponent
   ],
   imports: [
     BrowserModule,
